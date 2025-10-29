efc19c2126d9794651a5098a85add1e7e0ab6646
pranantoa <arianne.prananto@cosateq.com>
2025-10-29 18:07:08 +0100

    vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   );
